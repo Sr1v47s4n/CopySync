e1619f0b927467cfcf88dfe3f163ffbc8e8840a7
Sr1v47s4n <srivatsansk.dev@gmail.com>
2024-05-10 19:26:14 +0530

    modified:   CopySync/__pycache__/settings.cpython-312.pyc 	modified:   CopySync/settings.py 	new file:   db.sqlite3 	new file:   main/migrations/0013_alter_file_expiration_date_and_more.py 	new file:   main/migrations/__pycache__/0013_alter_file_expiration_date_and_more.cpython-312.pyc

--- CopySync/settings.py
+++ CopySync/settings.py
@@ -75,12 +75,9 @@ WSGI_APPLICATION = "CopySync.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "CopySync",
-        "USER": "postgres",
-        "PASSWORD": "root",
-        "HOST": "localhost",
-        "PORT": 5432,
+"ENGINE": "django.db.backends.sqlite3",
+"NAME": BASE_DIR / "db.sqlite3",
+
     }
 }
 
