eaaebb4ee67f295e1231e157bbae8e02066dcd39
Sr1v47s4n <srivatsansk.dev@gmail.com>
2024-05-13 18:13:33 +0530

    modified:   CopySync/__pycache__/settings.cpython-312.pyc 	modified:   CopySync/__pycache__/urls.cpython-312.pyc 	modified:   CopySync/urls.py 	modified:   main/__pycache__/urls.cpython-312.pyc 	modified:   main/__pycache__/views.cpython-312.pyc

--- CopySync/urls.py
+++ CopySync/urls.py
@@ -5,10 +5,9 @@ from django.conf.urls.static import static
 from main.views import error_404,error_500
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", include("main.urls")) # This line tells Django to include the urls.py file from the main app
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# handler404 = error_404
-# handler500 = error_500
+handler404 = error_404
+handler500 = error_500
